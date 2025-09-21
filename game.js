277e0e9e6425bfa54c1d4c060cc63204100facdc
Rehanon-Mackenzie <fairywings38@hotmail.com>
2025-09-21 09:26:51 +0100

    create game js variables

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-console.log("Hello world from quiz!");
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
+
+let currentQuestion =  {};
+let acceptingAnswers = true;
+let score = 0
+let questionCounter = 0;
+let availableQuestions = [];
